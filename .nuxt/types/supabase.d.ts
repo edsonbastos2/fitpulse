39ec7faefbaeb2f89946eaa3e8ee5e3614305964
edsonbastos2 <edsonbastos2@hotmail.com>
2026-04-07 22:01:21 -0300

    chore: adicionar .gitignore e remover arquivos de build do git

--- .nuxt/types/supabase.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('/tmp/fitpulse/.pnpm/@nuxtjs+supabase@1.6.2/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('/tmp/fitpulse/.pnpm/@nuxtjs+supabase@1.6.2/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('/tmp/fitpulse/.pnpm/@nuxtjs+supabase@1.6.2/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('/tmp/fitpulse/.pnpm/@nuxtjs+supabase@1.6.2/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
-}
